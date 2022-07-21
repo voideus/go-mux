7d59be46418c569600b58a861db38477284addb1
voideus <tbikash227@gmail.com>
2022-07-21 10:07:35 +0545

    Add auth middleware to route

--- api/routes/post_routes.go
+++ api/routes/post_routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/voideus/golang-mux-rest/api/middlewares"
 	"github.com/voideus/golang-mux-rest/controller"
 	"github.com/voideus/golang-mux-rest/infrastructure"
 )
@@ -8,18 +9,21 @@ import (
 type PostRoutes struct {
 	handler        infrastructure.Router
 	postController controller.PostController
+	authMiddleware middlewares.FirebaseAuthMiddleware
 }
 
 func NewPostRoutes(
 	handler infrastructure.Router,
 	postController controller.PostController,
+	authMiddleware middlewares.FirebaseAuthMiddleware,
 ) *PostRoutes {
 	return &PostRoutes{
 		handler:        handler,
 		postController: postController,
+		authMiddleware: authMiddleware,
 	}
 }
 
 func (pr PostRoutes) Setup() {
-	pr.handler.GET("/posts", pr.postController.GetPosts)
+	pr.handler.GET("/posts", pr.authMiddleware.Handle(), pr.postController.GetPosts)
 }
